21a6214dbc6db5c3b01341749bcbe6392e572d5c
ᴋᴀʙɪʀ々ꜱɪɴɢʜ <73753258+kabirsingh2004@users.noreply.github.com>
2022-10-14 15:16:43 +0530

    hover on links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,17 +8,13 @@ const Navbar = ({ page, setPage }) => {
         Tech Boy
       </a>
       <div className="links">
-        {[
-          "About US",
-          "Portfolio",
-          "Skills",
-          "Services",
-          "Contact US",
-        ].map((title, index) => (
-          <li key={index}>
-            <p onClick={() => setPage(title)}>{title}</p>
-          </li>
-        ))}
+        {["About US", "Portfolio", "Skills", "Services", "Contact US"].map(
+          (title, index) => (
+            <li key={index}>
+              <p onClick={() => setPage(title)} className="hover_line">{title}</p>
+            </li>
+          )
+        )}
       </div>
     </div>
   );
